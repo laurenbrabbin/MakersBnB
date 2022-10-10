b11583c613eb07ca51e82f8946fd3c79018d72d8
Tauqeer Shoaib <tauqeershoaib@gmail.com>
2022-10-10 17:01:57 +0100

    seeds done

---
+++ databases/seeds_hosts.sql
@@ -0,0 +1,4 @@
+TRUNCATE TABLE hosts RESTART INDENTITY CASCADE;
+INSERT INTO hosts (name, password, email) VALUES ('host1', 'password1', 'host1@email.com');
+INSERT INTO hosts (name, password, email) VALUES ('host2', 'password2', 'host2@email.com');
+INSERT INTO hosts (name, password, email) VALUES ('host3', 'password3', 'host3@email.com');
