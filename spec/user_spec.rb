f0b708b46257b015381a3eb02838ca62864bfc65
Aamir Khan <63204104+HoodieB@users.noreply.github.com>
2022-10-11 10:27:48 +0100

    Added BCrypt test to user_spec.rb

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -2,8 +2,15 @@ require 'users'
 
 describe '.user_create' do
   it 'creates a user' do
-    user = User.user_create(username: 'user1', password 'password1')
+    user = User.user_create(username: 'user4', password 'password4')
 
-    expect(user.username).to eq ('user1')
+    expect(user.username).to eq ('user4')
+    expect(user.password).to eq ('password4')
+  end
+
+  it 'encrypts the password using BCrypt' do
+    expect(BCrypt::Password).to receive (:create).with('password4')
+
+    user = User.create(username: 'user4', email: 'email4@email.com', password: 'password4')
   end
 end
