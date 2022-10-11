f75d31ce284518c7e9a97702a710f5478524128b
Aamir Khan <63204104+HoodieB@users.noreply.github.com>
2022-10-11 10:43:24 +0100

    Began users.rb integration of Bcrypt

--- lib/users.rb
+++ lib/users.rb
@@ -2,11 +2,19 @@ require 'pg'
 require 'bcrypt'
 
 class User
-  attr_reader :username, :password, :id
+  attr_reader :username, :password, :id, :email
 
-  def initialize(id:, username:, password:)
+  def initialize(id:, username:, password:, email:)
     @id = id
     @username = username
     @password = password
+    @email = email
   end
+
+  def self.create(username:, password:, email:)
+    encrypted_password = BCrypt::Password.create(password)
+
+    result = DatabaseConnection.query
+  end
+
 end
