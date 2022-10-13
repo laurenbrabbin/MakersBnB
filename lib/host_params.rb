2f716e244722235280e372a940818c7f84e66a12
Lauren Brabbin <laurenbrabbin@MASTUDENT-Lauren-Brabbin-C17G44KUQ6L4.local>
2022-10-13 09:34:13 +0100

    adding feature to check duplicate email and username for host sign up

--- lib/host_params.rb
+++ lib/host_params.rb
@@ -29,38 +29,33 @@ class HostParams
     @password.length < 8 || @password.count("0-9") == 0
   end 
 
-  #need to check using host.rb
+  def duplicate_username?
+    repo = HostRepository.new 
+    all_hosts = repo.all
+    duplicate_username = false
 
-  #def duplicate_username?
-    #repo = HostRepository.new 
-    #all_hosts = repo.all
-    #duplicate_username = false
+   all_hosts.each do |host|
+      if @username.downcase == host.username.downcase
+        duplicate_username = true
+      end
+    end
+    return duplicate_username
+ end
 
-   # all_hosts.each do |host|
-      #if @username.downcase == host.username.downcase
-       # duplicate_username = true
-      #end
-    #end
-    #return duplicate_username
- # end
+ def duplicate_email?
+  repo = HostRepository.new 
+  all_hosts = repo.all
+  duplicate_email = false
 
-  #need to check using host.rb
-
- # def duplicate_email?
-  #  repo = HostRepository.new 
-  #  all_hosts = repo.all
-  #  duplicate_email = false
-
-  #  all_hosts.each do |host|
-  #    if @email.downcase == host.email.downcase
-  #      duplicate_email = true
-  #    end
-  #  end
- #   return duplicate_email
- # end
+  all_hosts.each do |host|
+    if @email.downcase == host.email.downcase
+      duplicate_email = true
+    end
+  end
+   return duplicate_email
+ end
 
   def invaild_host_params?
-    invalid_email?  || weak_password? || name_contains_incorrect_characters? || username_contains_incorrect_characters? || password_contains_incorrect_characters?
-    # || duplicate_username? || duplicate_email?
+    duplicate_username? || duplicate_email? || invalid_email?  || weak_password? || name_contains_incorrect_characters? || username_contains_incorrect_characters? || password_contains_incorrect_characters?
   end
 end
